08265e005e5ecbef7598f67747dfe2a1943f8ac0
Victor H. Garcia <victorhugogarcia13d@gmail.com>
2019-10-02 02:52:38 -0500

    Add some styles to helloworld.html page

--- assets/js/helloworld/scripts.js
+++ assets/js/helloworld/scripts.js
@@ -3,7 +3,9 @@ function initialPageData() {
         type: "GET",
         url: "https://reqres.in/api/users?page=2",
         success: function(data) {
-          console.log(data.data);
+          for(i=0;i<data.data.length;i++) {
+            $('#character'+i).text(data.data[i].first_name + " " + data.data[i].last_name);
+          }
         }
       });
 }
