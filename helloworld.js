28312608c8ef32921035df75b1844d7cc58e7488
Victor H. Garcia <victorhugogarcia13d@gmail.com>
2019-10-07 23:12:02 -0500

    Serving HTML page from root URL

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,67 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response, function (e, res) {
-        file.serveFile('/helloworld.html', request, response);
-    });
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var checkMimeType = false;
+
+console.log("Starting web server at port: " + process.env.PORT || 3030);
+
+http.createServer( function(req, res) {
+
+	var now = new Date();
+
+    var filename = req.url != "/" ? req.url : "/helloworld.html";
+    console.log(filename);
+	var ext = path.extname(filename);
+	var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",
+		".js": "application/javascript",
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png",
+		".woff": "application/font-woff",
+		".woff2": "application/font-woff2"
+	};
+
+	var validMimeType = true;
+	var mimeType = validExtensions[ext];
+	if (checkMimeType) {
+		validMimeType = validExtensions[ext] != undefined;
+	}
+
+	if (validMimeType) {
+		localPath += filename;
+		fs.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, mimeType);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+
+	} else {
+		console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
+	}
+
+}).listen(process.env.PORT || 3030);
+
+function getFile(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			if (mimeType != undefined) {
+				res.setHeader("Content-Type", mimeType);
+			}
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
