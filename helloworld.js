1ef3ddd1d3eaa5657180deeb5b91c973677adbca
Victor H. Garcia <victorhugogarcia13d@gmail.com>
2019-10-07 22:38:02 -0500

    Serve helloworld.html from node

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,8 @@ var static = require('node-static');
 var file = new static.Server();
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
-    file.serve(request, response);
+    file.serve(request, response, function (e, res) {
+        fileServer.serveFile('/helloworld.html', request, response);
+    });
   }).resume();
 }).listen(process.env.PORT || 3000);
